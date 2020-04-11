e0bbeb3ef1f6e774612b9ecc5502c80e6e4f795d
Chintan Patel <chintan737@gmail.com>
2020-04-11 22:13:00 +0530

    Added Service interfaces and base entities

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -3,8 +3,11 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class PetType extends BaseEntity {
+public class BaseEntity implements Serializable {
 
+    private Long id;
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,13 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
-import java.time.LocalDate;
-
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
-public class Pet {
+public class Pet extends BaseEntity {
 	
 	private PetType petType;
 	private Owner owner;
