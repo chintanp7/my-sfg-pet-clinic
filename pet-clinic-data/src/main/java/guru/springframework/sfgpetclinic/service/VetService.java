27f988adf7e112359a7f1e83277a8f0f0853033b
chintanp7 <chintan737@gmail.com>
2020-04-12 19:33:59 +0530

    Removed extra imports

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/VetService.java
@@ -2,8 +2,6 @@ package guru.springframework.sfgpetclinic.service;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet, Long> {
 
 }
